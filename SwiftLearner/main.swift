b063afc634cee151ec38a2c49f041067b4308cca
Yusuf Turan <dryusufturan@gmail.com>
2021-02-03 02:43:35 +0300

    Add files via upload

---
+++ SwiftLearner/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftLearner
+//
+//  Created by Yusuf Turan on 3.02.2021.
+//
+
+import Foundation
+
+let number = Int.random(in: 1...500)
+for i in 1...number{
+	print("Hello \(i)")
+}
+
+
